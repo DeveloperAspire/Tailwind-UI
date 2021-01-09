3caa1a2d95d335aa07f6682cc61139fb3a01b936
Franklin <franklinokolie86@gmail.com>
2021-01-09 14:39:54 +0100

    Made Tailwind UI with Tailwind CSS
    Signed-off-by: Franklin <franklinokolie86@gmail.com>

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors:{
+      "darkblue": "#161E2E",
+      "ash": "#D1D5DB",
+      "litash":"#374151",
+      "white": "#ffff",
+      "litblue":"#4FD4EE",
+      "darkash": "#FCFCFD",
+      "litterblue": "#F3F4F6"
+    },
+    fontFamily:{
+      "rob":['Roboto', 'sans-serif']
+    },
+    backgroundImage:{
+      'site': "url('/img/site.png')"
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
